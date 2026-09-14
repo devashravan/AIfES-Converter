9b0eea08bca199579c47b2738fca6d1867322007
Shravan Deva <devashravan7@gmail.com>
2026-09-14 16:19:27 +0530

    Relax python_requires upper bound in setup.py
    Signed-off-by: Shravan Deva <devashravan7@gmail.com>

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 
-    python_requires=">=3.7, <3.10",
+    python_requires=">=3.7",
 
     install_requires=[
                       "numpy>=1.19",
